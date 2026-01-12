13c26a476bf17790a65a92bb0fc9f3206503e6e5
ingray11 <uiio19991201@gmail.com>
2026-01-12 19:59:52 +0900

    fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,13 +28,14 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 PROJECT_APPS = [
     "apps.user",
+    "apps.Post"
     "apps.interactions",
 ]
 
