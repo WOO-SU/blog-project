62b2d71a750b70b8fd5d53da7142d2a7622321d6
Jaesung Lee <ljaesung1130@gmail.com>
2026-01-12 15:04:13 +0900

    fixed urls

--- backend/apps/urls.py
+++ backend/apps/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
-    # path("user/", include("apps.user.urls")),
+    path("user/", include("apps.user.urls")),
+    path("posts/", include("apps.Post.urls")),
+    path("interactions/", include("apps.interactions.urls")),
 ]
